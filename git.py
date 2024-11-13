5823ae11bcd8017851bd62cc97c5e5303233f01f
xmk472 <bab63299@gmail.com>
2024-11-13 08:28:29 +0000

    Prog python

---
+++ git.py
@@ -0,0 +1,15 @@
+#coding:utf-8
+"""
+Ici nous parlons des condition
+en langage PYthon
+"""
+moy_ad = 18.35
+moy = input("Donner la moyenne que vous avez obtenu cette annee")
+
+moy = int(moy)
+
+if moy >= moy_ad :
+    print("Vous avez maintenu le cap , bravo")
+
+else:
+    print("Vous ne vous etes pas confirme et c'est pas bon du tout tachez de vous en souvenir ")
